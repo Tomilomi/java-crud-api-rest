e116b8c1b47c9e9e91e4edc27f5deb89e02e0676
tomilomi <soulcrack26196@gmai.com>
2025-01-24 15:16:15 -0300

    v1

---
+++ src/main/java/com/tomilomi/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,10 @@
+package com.tomilomi.apirest.apirest.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tomilomi.apirest.apirest.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long>{
+    
+
+
+}
